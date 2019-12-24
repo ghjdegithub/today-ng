2cb10bd367a94329bd8013ebb1bf027ebf0ec9ee
ghjdegithub <303690073@qq.com>
2019-12-24 18:15:46 +0800

    ste

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,17 +2,29 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MainComponent} from './main.component';
 import {MainRoutingModule} from './main-routing.module';
-import {NzButtonModule, NzLayoutModule} from 'ng-zorro-antd';
+import {
+  NzButtonModule, NzFormModule,
+  NzIconModule, NzInputModule,
+  NzLayoutModule,
+  NzMenuModule,
+  NzModalModule
+} from 'ng-zorro-antd';
 import { LeftControlComponent } from './left-control/left-control.component';
+import { ListComponent } from './left-control/list/list.component';
 
 
 @NgModule({
-  declarations: [MainComponent, LeftControlComponent],
+  declarations: [MainComponent, LeftControlComponent, ListComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
-    NzLayoutModule,
     NzButtonModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzModalModule,
+    NzFormModule,
+    NzInputModule,
   ]
 })
 export class MainModule {
