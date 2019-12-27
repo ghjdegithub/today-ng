5ab28da0027dbeed6fe30129187e43011c825a91
ghjdegithub <303690073@qq.com>
2019-12-27 13:42:15 +0800

    init

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 ng build --prod;
 cp ./404.html ./dist/today-ng/404.html
+cd ./dist/today-ng
+git add .
+git commit -m "init project"
+git push origin gh-pages
 #git add dist --force;
 #git commit -m "demo: build deml";
 #git subtree push --prefix dist/today-ng origin gh-pages;
